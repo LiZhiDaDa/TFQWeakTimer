e0a366bfe8e23825c5ffc418fcf87c046cd4e1c1
王立志 <1226169349@qq.com>
2019-01-18 11:42:21 +0800

    Gcd 创建定时器

--- TFQWeakTimer/TFQGCDTimer/TFQGCDTimer.m
+++ TFQWeakTimer/TFQGCDTimer/TFQGCDTimer.m
@@ -21,6 +21,23 @@ static NSMutableDictionary *timerDict;
 }
 
 + (NSString *)schedleTask:(void (^)(void))task interval:(NSTimeInterval)interval repeat:(BOOL)repeat async:(BOOL)async reuseIdentifier:(NSString *)identifier{
+    dispatch_queue_t queue = async ? dispatch_get_global_queue(0, 0) : dispatch_get_main_queue();
+    //穿件定时器
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    //开始时间
+    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, 0*NSEC_PER_SEC);
+    //设置各种时间
+    dispatch_source_set_timer(timer, start, interval*NSEC_PER_SEC, 0);
+    //设置回调
+    dispatch_source_set_event_handler(timer, ^{
+        task();
+        if(!repeat){
+            [self cancelTimer:identifier];
+        }
+    });
+    //启动定时器
+    dispatch_resume(timer);
+    timerDict[identifier] = timer;
     return identifier;
 }
 
